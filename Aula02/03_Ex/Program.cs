e154dea183bda56a81a2fd8ac98c3ba7ec87c57d
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2021-11-20 15:55:19 -0300

    Exercicio 03 Aula 02

--- Aula02/03_Ex/Program.cs
+++ Aula02/03_Ex/Program.cs
@@ -10,9 +10,80 @@
  * int/uint
  * long/ulong
  * 
- * a) Faça um program console que declare variáveis dos tipos acima descritos
- * b) Atribua valores iniciais para as variáveis e tente atribuir valores de variaveis de um tipo para outro tipo. 
- * c) Sempre printando na tela o resultado do teste quando possivel
 */
 
-Console.WriteLine("Hello, World!");
+// a) Faça um program console que declare variáveis dos tipos acima descritos
+
+short a;
+ushort b;
+int c;
+uint d;
+long e;
+ulong f;
+
+// b) Atribua valores iniciais para as variáveis e tente atribuir valores de variaveis de um tipo para outro tipo. 
+
+a = 10;
+b = 20;
+c = 30;
+d = 12;
+e = 50;
+f = 2;
+
+a = (short)a;
+b = (ushort)(short)a;
+c = (short)a;
+f = (ulong)(short)a;
+d = (uint)(short)a;
+e = (short)a;
+
+Console.WriteLine("Valor de 'a': " + a);
+Console.WriteLine("Valor de 'b': " + b);
+Console.WriteLine("Valor de 'c': " + c);
+Console.WriteLine("Valor de 'd': " + d);
+Console.WriteLine("Valor de 'e': " + e);
+Console.WriteLine("Valor de 'f': " + f);
+
+a = 10;
+b = 20;
+c = 30;
+d = 12;
+e = 50;
+f = 2;
+
+a = (short)(int)c;
+b = (ushort)(int)c;
+c = (int)c;
+d = (uint)(int)c;
+e = (int)c;
+f = (ulong)(int)c;
+
+Console.WriteLine("Valor de 'a': " + a);
+Console.WriteLine("Valor de 'b': " + b);
+Console.WriteLine("Valor de 'c': " + c);
+Console.WriteLine("Valor de 'd': " + d);
+Console.WriteLine("Valor de 'e': " + e);
+Console.WriteLine("Valor de 'f': " + f);
+
+a = 10;
+b = 20;
+c = 30;
+d = 12;
+e = 50;
+f = 2;
+
+a = (short)(long)e;
+b = (ushort)(long)e;
+c = (int)(long)e;
+f = (ulong)(long)e;
+d = (uint)(long)e;
+e = (long)e;
+
+// c) Sempre printando na tela o resultado do teste quando possivel
+
+Console.WriteLine("Valor de 'a': " + a);
+Console.WriteLine("Valor de 'b': " + b);
+Console.WriteLine("Valor de 'c': " + c);
+Console.WriteLine("Valor de 'd': " + d);
+Console.WriteLine("Valor de 'e': " + e);
+Console.WriteLine("Valor de 'f': " + f);
