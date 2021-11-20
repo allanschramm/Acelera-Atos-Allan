5726c94b0b7a87eb69ac35eeebbfca57b5333b23
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2021-11-20 16:53:09 -0300

    Exercicio 07 Aula 02

--- Aula02/07_Ex/Program.cs
+++ Aula02/07_Ex/Program.cs
@@ -4,4 +4,10 @@
  * 7) Construa um aplicativo console que exiba na saida os 10 primeiros multiplos de 2, usando operadores de atribuição adequados.
 */
 
-Console.WriteLine("Hello, World!");
+for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
