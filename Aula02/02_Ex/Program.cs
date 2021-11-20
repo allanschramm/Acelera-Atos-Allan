6129d1b11c5227dfc58d55832aed01752d3cab4e
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2021-11-20 15:32:17 -0300

    Exercicio 02 Aula 02

--- Aula02/02_Ex/Program.cs
+++ Aula02/02_Ex/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * 2) O que acontece se eu colocar um valor maior do que o suportado em uma variável byte ou sbyte ?
+ * 2) O que acontece se eu colocar um valor maior do que o suportado em uma variável byte ou sbyte?
  * 
  * byte (8 bit)
  * 0 ate 255
@@ -10,10 +10,37 @@
  * 
  * Faça usando tres formas que são:
  * 
- * a) Atribua diretamente um valor acima do limite para as variáveis.
- * b) Atribua um valor acima do limite para uma variável int depois faça um cast para as variáveis byte e sbyte.
- * c) Recupere valores do usuário para atribuir para as variáveis. Quando executar o programa
- * d) Insira valores acima do limite das variáveis.
 */
 
-Console.WriteLine("Hello, World!");
+// a) Atribua diretamente um valor acima do limite para as variáveis.
+
+int a = 300;
+
+// byte b = 300; // CS0031: Constant value '300' cannot be converted to a 'byte'
+
+Console.WriteLine(a);
+
+// b) Atribua um valor acima do limite para uma variável int depois faça um cast para as variáveis byte e sbyte.
+
+byte b = (byte)a;
+sbyte c = (sbyte)a;
+
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+// c) Recupere valores do usuário para atribuir para as variáveis. Quando executar o programa
+
+Console.WriteLine("Digite um valor para a variavel Byte: ");
+b = byte.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um valor para a variavel SByte: ");
+c = sbyte.Parse(Console.ReadLine());
+
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//  d) Insira valores acima do limite das variáveis.
+//      Unhandled exception.System.OverflowException: Value was either too large or too small for a signed byte.
+//          at System.Number.ThrowOverflowException(TypeCode type)
+//          at System.SByte.Parse(String s)
+//          at Program.<Main>$(String[] args) in E:\Dev\CSharp\AceleraAtos\Aula02\02_Ex\Program.cs:line 37
